621b56d7646d9bfa4ea1efb74ae25808b5cd9c80
LIN-Tianyuan <47159399+LIN-Tianyuan@users.noreply.github.com>
2025-03-23 16:10:20 +0100

    Week 5

--- Chapter3/Project/game.py
+++ Chapter3/Project/game.py
@@ -11,4 +11,14 @@ else:
     print("Les deux côtés sont à l'égalité.")
 """
 
-print(random.randint(1, 10))
+# print(random.randint(1, 10))
+player = int(input("Que voulez-vous jouer? Pierre(1)/Ciseaux(2)/Papier(3)"))
+computer = random.randint(1, 3)
+print("Ordinateur: " + str(computer))
+if player == computer:
+    print("Les deux côtés sont à égalité.")
+elif (player == 1 and computer == 2) or (player == 2 and computer == 3) or (player == 3 and computer == 1):
+    print("Le joueur gagne.")
+else:
+    print("L'ordinateur gagne.")
+
---
+++ Chapter4/Course/exercise2.py
@@ -0,0 +1,6 @@
+i = 0
+sum_number = 0
+while i <= 100:
+    sum_number = i + sum_number
+    i = i + 1
+print(sum_number)
